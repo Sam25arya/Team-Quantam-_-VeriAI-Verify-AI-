092d4230a0bee5fb5fc235bb76f427883dcaf582
Arya Sawant <aryasawant250908@gmail.com>
2026-04-14 17:16:08 +0530

    Add home and predict routes to app.py

--- app.py
+++ app.py
@@ -9,3 +9,20 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # Ensure upload folder exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+@app.route("/predict",method=["POST"])
+def predicit():
+  if 'file' not in request.file:
+    return "No file part"
+
+file = request.files["file"]
+
+if file.filename == '':
+    return "No selected file"
+
+ 
+ 
