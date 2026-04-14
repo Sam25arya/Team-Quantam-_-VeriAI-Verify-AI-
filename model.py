067716f023f2de1089f6ad40cf933f617b4eb321
NidhiPandey18 <pandeynidhi0501@gmail.com>
2026-04-14 17:24:18 +0530

    Implement document detection in model.py
    Add document detection functionality based on image contrast

--- model.py
+++ model.py
@@ -12,3 +12,14 @@ elif mean < 180:
   return "⚠️Suspicious"
 else:
   return "❌AI Generated"
+  
+def detect_document(path):
+  #convert grayscale to check contrast
+  image = Image.open(path).convert("L")
+  stat = ImageStat.Stat(image)
+  std_dev = stat.stddev[0]
+
+if std_dev > 65:
+  return "📄Document Detected"
+else:
+  return "🖼️Standard Image"
